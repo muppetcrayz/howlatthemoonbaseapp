963102b6e87098abf0ef0ee7dd2d12de2ed79b13
Sage Conger <sconger@capital.edu>
2019-03-04 23:08:24 -0500

    carousel

--- howlatthemoon/View Controllers/DetailViewController.swift
+++ howlatthemoon/View Controllers/DetailViewController.swift
@@ -9,36 +9,62 @@
 import UIKit
 
 class DetailViewController: HowlAtTheMoonViewController {
-    let scrollView = UIScrollView(frame: .zero)
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let newCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        newCollectionView.usesAutoLayout = true
+        newCollectionView.backgroundColor = .clear
+        newCollectionView.isScrollEnabled = true
+        return newCollectionView
+    }()
+//    let scrollView = UIScrollView(frame: .zero)
 
     let image = UIImage(named: "exampleDetailImage")!
 
-    let previousButton = UIButton(type: .system)
-    let nextButton = UIButton(type: .system)
+    let previousButton = UIButton(type: .custom)
+    let nextButton = UIButton(type: .custom)
+    
+    let cellId = "ImageCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        with(scrollView) {
-            $0.isPagingEnabled = true
-
-            (0...8).forEach { i in
-
-                let imageView = UIImageView(image: image)
-                
-                scrollView.addSubview(imageView)
-
-                imageView.snp.makeConstraints {
-                    $0.leading.equalTo(scrollView).offset(CGFloat(i) * (image.size.width + 25))
-                    $0.top.equalTo(scrollView)
-                }
-            }
-
+//        with(scrollView) {
+//            $0.isPagingEnabled = true
+//
+//            (0...8).forEach { i in
+//
+//                let imageView = UIImageView(image: image)
+//
+//                scrollView.addSubview(imageView)
+//
+//                imageView.snp.makeConstraints {
+//                    $0.width.equalTo(scrollView).multipliedBy(0.3)
+//                    $0.height.equalTo(imageView.snp.width).multipliedBy(1.4606060606)
+//                    $0.centerX.equalTo(scrollView).multipliedBy(0.3 + (CGFloat(i) * 0.75) + ((CGFloat(i) / 3) * -0.1))
+//                    $0.top.equalTo(scrollView)
+//                }
+//            }
+//
+//            view.addSubview($0)
+//            $0.snp.makeConstraints {
+//                $0.top.equalTo(view.safeAreaLayoutGuide).offset(225)
+//                $0.centerX.equalTo(view.safeAreaLayoutGuide)
+//                $0.leading.equalTo(view.safeAreaLayoutGuide).offset(150)
+//                $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-150)
+//                $0.bottom.equalTo(view)
+//            }
+//        }
+        
+        with(collectionView) {
             view.addSubview($0)
+            $0.delegate = self
+            $0.dataSource = self
+            $0.register(ImageViewCell.self, forCellWithReuseIdentifier: cellId)
+            
             $0.snp.makeConstraints {
                 $0.top.equalTo(view.safeAreaLayoutGuide).offset(225)
-                $0.centerX.equalTo(view.safeAreaLayoutGuide)
-                $0.width.equalTo(3 * (image.size.width + 25))
                 $0.leading.equalTo(view.safeAreaLayoutGuide).offset(150)
                 $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-150)
                 $0.bottom.equalTo(view)
@@ -46,9 +72,9 @@ class DetailViewController: HowlAtTheMoonViewController {
         }
 
         with(previousButton) {
-            $0.setTitle("Previous", for: .normal)
+            $0.setImage(UIImage(named: "left"), for: .normal)
             $0.addAction(for: .touchUpInside) {
-                self.scrollView.setContentOffset(CGPoint(x: self.scrollView.contentOffset.x - (3 * (self.image.size.width + 25)), y: self.scrollView.contentOffset.y), animated: true)
+                self.collectionView.setContentOffset(CGPoint(x: self.collectionView.contentOffset.x - self.collectionView.bounds.width, y: self.collectionView.contentOffset.y), animated: true)
             }
 
             view.addSubview($0)
@@ -56,13 +82,14 @@ class DetailViewController: HowlAtTheMoonViewController {
             $0.snp.makeConstraints {
                 $0.leading.equalTo(view.safeAreaLayoutGuide).offset(50)
                 $0.centerY.equalTo(view.safeAreaLayoutGuide)
+                $0.width.height.equalTo(100)
             }
         }
 
         with(nextButton) {
-            $0.setTitle("Next", for: .normal)
+            $0.setImage(UIImage(named: "right"), for: .normal)
             $0.addAction(for: .touchUpInside) {
-                self.scrollView.setContentOffset(CGPoint(x: self.scrollView.contentOffset.x + (3 * (self.image.size.width + 25)), y: self.scrollView.contentOffset.y), animated: true)
+                self.collectionView.setContentOffset(CGPoint(x: self.collectionView.contentOffset.x + self.collectionView.bounds.width, y: self.collectionView.contentOffset.y), animated: true)
             }
 
             view.addSubview($0)
@@ -70,6 +97,7 @@ class DetailViewController: HowlAtTheMoonViewController {
             $0.snp.makeConstraints {
                 $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-50)
                 $0.centerY.equalTo(view.safeAreaLayoutGuide)
+                $0.width.height.equalTo(100)
             }
         }
         // Do any additional setup after loading the view.
@@ -86,4 +114,78 @@ class DetailViewController: HowlAtTheMoonViewController {
     }
     */
 
+}
+
+extension DetailViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! ImageViewCell
+        
+        cell.backgroundColor = .clear
+        
+        cell.image = UIImage(named: "exampleDetailImage")
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfItemsPerRow: CGFloat = 3.0
+        
+        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+        let totalSpace = flowLayout.sectionInset.left
+            + flowLayout.sectionInset.right
+            + (flowLayout.minimumInteritemSpacing * numberOfItemsPerRow - 1)
+        let size = Int((collectionView.bounds.width - totalSpace) / numberOfItemsPerRow)
+        return CGSize(width: size, height: Int(collectionView.bounds.height))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        let totalCellWidth = 80 * collectionView.numberOfItems(inSection: 0)
+        let totalSpacingWidth = 10 * (collectionView.numberOfItems(inSection: 0) - 1)
+        
+        let leftInset = (collectionView.layer.frame.size.width - CGFloat(totalCellWidth + totalSpacingWidth)) / 2
+        let rightInset = leftInset
+        
+        return UIEdgeInsets(top: 0, left: leftInset, bottom: 0, right: rightInset)
+        
+    }
+}
+
+class ImageViewCell: UICollectionViewCell {
+    let imageView = UIImageView()
+    
+    var image: UIImage? {
+        didSet {
+            imageView.image = image
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        with(imageView) {
+            $0.contentMode = .scaleAspectFit
+            
+            addSubview($0)
+            
+            $0.snp.makeConstraints {
+                $0.edges.equalTo(self)
+            }
+        }
+
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
